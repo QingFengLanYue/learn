366a640ba2f36ce7e2f9e16f03ec2d7e679e9af1
luokesasi <919880391@qq.com>
2020-12-11 19:53:21 +0800

    cs

---
+++ test01/zhumian.py
@@ -0,0 +1,13 @@
+from test01.lilv import lvv
+
+if __name__ == '__main__':
+    year=30
+    l=5.45
+    amount=97
+    lvv.mian_lv(year,l,amount)
+
+
+
+
+l1=2202.70+4390.55+4385.61+4380.65+4375.67+4370.67+4365.65+4360.60	+4355.52+4350.43+ 4345.32
+print(l1)
